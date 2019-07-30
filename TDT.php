b8f28f311b11668746c403bd49e07dc153219309
TDT <52957439+trandinhthu@users.noreply.github.com>
2019-07-30 14:17:33 +0700

    created

---
+++ TDT.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php  
+$x = 1;
+ 
+while($x <= 10) {
+  echo "hello world <br>";
+  $x++;
+} 
+?>
+</body>
+</html>
